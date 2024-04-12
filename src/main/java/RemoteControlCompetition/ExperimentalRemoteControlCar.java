ebd83e6bf37d6b4815b3352ad166c2f99c730273
Mr. Orleond <mr.orleond@gmail.com>
2024-04-12 12:56:33 +0300

    Exercise 10

---
+++ src/main/java/RemoteControlCompetition/ExperimentalRemoteControlCar.java
@@ -0,0 +1,11 @@
+package main.java.RemoteControlCompetition;
+
+public class ExperimentalRemoteControlCar implements RemoteControlCar {
+    private int distance;
+    public void drive() {
+        distance += 20;
+    }
+    public int getDistanceTravelled() {
+        return distance;
+    }
+}
---
+++ src/main/java/RemoteControlCompetition/RemoteControlCar.java
@@ -0,0 +1,6 @@
+package main.java.RemoteControlCompetition;
+
+public interface RemoteControlCar {
+    void drive();
+    int getDistanceTravelled();
+}
