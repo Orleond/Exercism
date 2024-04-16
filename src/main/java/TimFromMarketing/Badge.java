789ceb2ffa191154fa868e9df9f9a9c19ec80afc
Mr. Orleond <mr.orleond@gmail.com>
2024-04-16 11:47:17 +0300

    Exercise 17. Better version

--- src/main/java/TimFromMarketing/Badge.java
+++ src/main/java/TimFromMarketing/Badge.java
@@ -2,10 +2,9 @@ package main.java.TimFromMarketing;
 
 public class Badge {
     public String print(Integer id, String name, String department) {
-        if (department == null)
-            return id == null ? String.format("%s - OWNER", name)
-                    : String.format("[%s] - %s - OWNER", id, name);
-        return id == null ? String.format("%s - %s", name, department.toUpperCase())
-            : String.format("[%s] - %s - %s", id, name, department.toUpperCase());
+
+        return String.format("%s%s - %s", id == null ? "" : "[" + id + "] - ",
+                name,
+                department == null ? "OWNER" : department.toUpperCase());
     }
 }
